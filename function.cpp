7a7b00660bb40708f5cc7fc130bd58910f7dcfbc
Johnny <johny60207@gmail.com>
2014-12-11 18:19:28 +0800

    Link file ok, makefile

--- function.cpp
+++ function.cpp
@@ -1,8 +1,9 @@
-#include <iostream> 
-#include <stdio.h>
-#include <map>
-
 #include "function.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
 
 int threshold;
 // List of mapper matrix
@@ -14,9 +15,109 @@ vector<int> total_utilities;
 // Sorted sequence
 vector<vector<int> > sequences;
 
-int main(){
-    return 0;
+void set_args(vector<map<int, vector<Entry> > > mx, vector<vector<int> > seq){
+    matrices = mx;
+    sequences = seq;
 }
+
+void test(){
+    PrintMatrix(matrices, sequences);
+}
+
+vector<map<int, vector<Entry> > > reader(char *fileName, vector<vector<int> > &sequences) {
+    vector<map<int, vector<Entry> > > matrix;
+    char buf[SIZE];
+    char *token;
+    fstream file;
+
+    file.open(fileName, ios::in);
+
+    if (!file) {
+        cout << "Can't open file!" << endl;
+        exit(1);
+    }
+
+    vector<vector<string> > tmpVector;
+    
+    // Split sequences by ';'
+    while (file.getline(buf, sizeof(buf))) {
+        token = strtok(buf, ";");
+        vector<string> s;
+        while (token != NULL) {
+            string tmp = string(token);
+            s.push_back(tmp);
+            token = strtok(NULL, ";");
+        }
+        buf[0] = 0;
+        tmpVector.push_back(s);
+    }
+    
+    // Parse each transaction
+    int idx = 0;
+    for (vector<vector<string> >::iterator v_iter = tmpVector.begin(); v_iter != tmpVector.end(); v_iter++, idx++) 
+    {
+        vector<string>::iterator iter = v_iter->begin();
+        char tmp[100];
+        // Split header
+        strcpy(tmp, iter->c_str());
+        int col = atoi(strtok(tmp, ",")); 
+        int row = atoi(strtok(NULL, ","));
+        tmp[0] = 0;
+        iter++;
+
+        
+        // Split pattern
+        strcpy(tmp, iter->c_str());
+        vector<int> p;
+        char *token;
+        token = strtok(tmp, ",");
+
+        while (token!= NULL) {
+            p.push_back(token[0] - '0');
+            token = strtok(NULL, ",");
+        }
+
+        iter++;
+
+        // Maintain pattern list
+        sequences.push_back(p);
+
+        // Split itemset
+        map<int, vector<Entry> > emap;
+        for (int i = 0; i != row; i++) 
+        {
+            vector<Entry> entry;
+            for (int j = 0; j != col; j++) 
+            {
+                Entry e;
+                strcpy(tmp, (iter + i + j*row)->c_str());
+                e.self = atoi(strtok(tmp, ","));
+                e.remain = atoi(strtok(NULL,","));
+                entry.push_back(e);
+            }
+            emap[p[i]] = entry;
+        }
+        matrix.push_back(emap);
+    }
+
+
+    return matrix;
+}
+
+void PrintMatrix(vector<map<int, vector<Entry> > > matrix, vector<vector<int> > sequences) {
+    for (int i = 0; i != matrix.size(); i++) 
+    {
+        for (int j = 0; j != sequences[i].size(); j++) 
+        {
+            cout << (char)('0' + sequences[i][j]) << " ";
+            for (vector<Entry>::iterator iter = matrix[i][sequences[i][j]].begin(); iter != matrix[i][sequences[i][j]].end(); iter++) 
+                cout << "(" << iter->self << "," << iter->remain << ") ";
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
+
 
 // Calculate the utility of current pattern after concating the new pattern.
 vector<int> cal_utility(map<int, vector<Entry> > matrix, int pattern, vector<int> pattern_index, vector<int> utility, int *max_utility) {
